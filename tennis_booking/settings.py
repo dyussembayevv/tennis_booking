b170cf0813df42078a3ef43d7c11934da9be2020
student <student@iab.kz>
2025-08-19 15:11:37 +0600

    Troubleshooting 2

--- tennis_booking/settings.py
+++ tennis_booking/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
